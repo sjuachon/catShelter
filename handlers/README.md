2a60c57f4c0ce362650da5c3a21d89571235c00d
sjuachon <sjuachon@gmail.com>
2021-07-24 13:02:28 -0700

    .

---
+++ handlers/README.md
@@ -0,0 +1,4 @@
+# Cat Shelter App and Github Practice
+This is information about our project
+
+Please run "npm install" toget started.  Then run "npm start".
---
+++ handlers/cat.js
@@ -0,0 +1,7 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('queryString');
+const formidable = require('formidable');
+const breeds = require('../data/breeds');
+const cats = require('../data/cats');
